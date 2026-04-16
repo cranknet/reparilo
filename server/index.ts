ddc3d01de39e22048548e2bd090bfb487cb67d5a
Bechar <cn.b@outlook.com>
2026-04-16 05:49:06 +0100

    feat(jobs): serve uploaded photos in dev mode

--- server/index.ts
+++ server/index.ts
@@ -53,6 +53,14 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+if (process.env.NODE_ENV !== "production") {
+  await app.register(staticPlugin, {
+    root: path.resolve("uploads"),
+    prefix: "/api/uploads/",
+    decorateReply: false,
+  });
+}
+
 process.on("uncaughtException", (err) => {
   app.log.fatal({ err }, "Uncaught exception");
   process.exit(1);
